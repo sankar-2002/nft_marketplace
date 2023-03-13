767c89f33044e06166dac80b488098923e1bedec
Sankaranarayanan Sengunther <shankar.muthusamy4@gmail.com>
2023-03-13 23:34:24 +0530

    done with the navbar starting footer

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -69,6 +69,7 @@ const Navbar = () => {
   const { theme, setTheme } = useTheme();
   const router = useRouter();
   const [active, setActive] = useState('Explore NFTs');
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white
@@ -124,6 +125,32 @@ const Navbar = () => {
         </div>
 
       </div>
+
+      <div className="hidden md:flex ml-2">
+        {isOpen ? (<Image src={images.cross} objectFit="contain" width={20} height={20} alt="close" onClick={() => setIsOpen(false)} className={theme === 'light' && 'filter invert'} />)
+          : (<Image src={images.menu} objectFit="contain" width={25} height={25} alt="menu" onClick={() => setIsOpen(true)} className={theme === 'light' && 'filter invert'} />)}
+
+        {isOpen && (
+        <div className="fixed inset-0 top-65 dark:bg-nft-dark bg-white z-10 nav-h flex justify-between flex-col">
+          <div className="flex-1 p-4">
+            <MenuItems
+              active={active}
+              setActive={setActive}
+              isMobile
+            />
+          </div>
+
+          <div className="p-4 border-t dark:border-nft-black-1 border-nft-gray-1">
+            <ButtonGroup setActive={setActive} router={router} />
+
+          </div>
+
+        </div>
+
+        )}
+      </div>
+
+      {/* here if the theme is light then invert the filter of icon */}
 
     </nav>
   );
