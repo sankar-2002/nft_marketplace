e35fb0e2b4f240dc8641ad3a2f2cb1352ed81e1c
Sankaranarayanan Sengunther <shankar.muthusamy4@gmail.com>
2023-03-18 18:21:12 +0530

    done with hot bids/home page complete

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from 'react';
 import Image from 'next/image';
 import { useTheme } from 'next-themes';
 
-import { Banner, CreatorCard } from '../components';
+import { Banner, CreatorCard, NFTCard } from '../components';
 import images from '../assets';
 import { makeId } from '../utils/makeId';
 
@@ -91,6 +91,38 @@ const Home = () => {
               </div>
             </div>
           </h1>
+        </div>
+
+        <div className="mt-10">
+          <div className="flexBetween mx-4 xs:mx-0 minlg:mx8 sm:flex-col sm:items-start">
+            <h1 className="flex-1 before:first:font-poppins dark:text-white
+        text-nft-black-1 text-2xl
+         minlg:text-4xl font-semibold sm:mb-4"
+            > Hot Bids
+            </h1>
+
+            <div>SearchBar</div>
+
+          </div>
+
+          <div className="mt-3 w-full flex flex-wrap justify-start md:justify-center">
+
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
+              <NFTCard
+                key={`nft-${i}`}
+                nft={{
+                  i,
+                  name: `Nifty NFT ${i}`,
+                  price: (10 - i * 0.534).toFixed(2),
+                  seller: `0x${makeId(3)}...${makeId(4)}`,
+                  owner: `0x${makeId(3)}...${makeId(4)}`,
+                  description: 'Cool NFT on Sale',
+                }}
+              />
+            ))}
+
+          </div>
+
         </div>
 
       </div>
