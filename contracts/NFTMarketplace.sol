45186a279769a158b2905808dbff4d42e063cd37
Sankaranarayanan Sengunther <shankar.muthusamy4@gmail.com>
2023-03-23 15:20:38 +0530

    solidity contd...

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -41,8 +41,93 @@ contract NFTMarketPlace is ERC721URIStorage {
     );
 
     //here it says that the owner is the one who deploys the contract(we get from from msg.sender)
-    
+
     constructor() {
         owner = payable(msg.sender);
+    }
+
+    function updateListingPrice (uint _listingPrice) public payable {
+        require(owner == msg.sender, "Only marketplace Owner can update the listing price"); //making sure you are the owner
+
+        listingPrice = _listingPrice; //updating the listing price...
+    }
+
+    //view means it does not handle any logic it just returns some value
+
+    function getListingPrice() public view returns (uint256) {
+        return listingPrice;
+    }
+
+    function createToken(string memory tokenURI, uint256 price) public payable returns (uint) {
+        _tokenIds.increment();  //incrementing token ids
+        uint256 newTokenId = _tokenIds.current(); 
+        _mint(msg.sender, newTokenId); //minting using newtokenId
+        _setTokenURI(newTokenId, tokenURI); //setting tokenURI
+
+        createMarketItem(newTokenId, price); //listing the token in our marketPlace
+
+        return newTokenId;
+    }
+
+    function createMarketItem (uint256 tokenId, uint256 price) private {
+        //condition checks....before creating marketItem
+
+        require(price > 0, "Price must be at least 1");
+        require(msg.value == listingPrice, "Price must be equal to listing Price");
+
+        //creating market Item...
+
+        idToMarketItem[tokenId] = MarketItem(
+            tokenId,
+            payable(msg.sender), //current address (owner)
+            payable(address(this)), //address of the person trying to create a marketItem
+            price,
+            false
+        );
+
+        //transfering ownership....
+
+        _transfer(msg.sender, address(this), tokenId);
+
+      //as we r already listening in the event now we'll emit that event
+        emit MarketItemCreated(tokenId, msg.sender, address(this), price, false);
+
+    }
+
+    function resellToken(uint256 tokenId, uint256 price) public payable {
+        require(idToMarketItem[tokenId].owner == msg.sender, "Only item owner can perform this operation");
+        require(msg.value == listingPrice, "Price must be equal to listing price");
+
+     // changing the status of the item as we r going to resell
+        idToMarketItem[tokenId].sold = false;
+        idToMarketItem[tokenId].price = price;
+        idToMarketItem[tokenId].seller = payable(msg.sender);
+        idToMarketItem[tokenId].owner = payable(address(this));
+
+        _itemsSold.decrement();
+
+        _transfer(msg.sender, address(this), tokenId); //transfering ownership....
+    }
+
+      //selling token using nftMarketPlace
+
+    function createMarketSale(uint256 tokenId) public payable {
+        uint price = idToMarketItem[tokenId].price;
+
+        require(msg.value == price, "Please submit the asking price in order to complete the purchase");
+
+        idToMarketItem[tokenId].owner = payable(msg.sender); // setting owner
+        idToMarketItem[tokenId].sold = true;
+        idToMarketItem[tokenId].seller = payable(address(0));  //here seller is nftmarketPlace
+
+
+        _itemsSold.increment();
+
+        _transfer(address(this), msg.sender, tokenId);
+
+        payable(owner).transfer(listingPrice); //owner get's the listing price we declared above
+        payable(idToMarketItem[tokenId].seller).transfer(msg.value); //transferring the price of nft from buyer to seller
+
+
     }
 }
